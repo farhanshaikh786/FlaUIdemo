ad46a3b02bd88e09f07933549630e76cfc9fd43d
farhan shaikh <shaikh.farhan2009@gmail.com>
2018-07-10 10:34:24 +0100

    Add project files.

---
+++ FlaUItest.demo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FlaUItest.demo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Amazon.com")]
+[assembly: AssemblyProduct("FlaUItest.demo")]
+[assembly: AssemblyCopyright("Copyright © Amazon.com 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bd6fabc9-09de-4f17-93bc-a0b365b14a57")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FlaUItest.demo/UnitTest1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.WindowItems;
+using System.Diagnostics;
+using TestStack.White;
+using NUnit.Framework;
+using System.Threading;
+using TestStack.White.Factory;
+
+namespace FlaUItest.demo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+          public Application app;
+
+        [TestInitialize]
+        public void before()
+        {
+            string apppath = @"D:/Users/farhanShaikh/Desktop/Telerik OutlookInspiredApp for WPF.appref-ms";
+            var prc = new ProcessStartInfo(apppath);
+            app = Application.Launch(prc);
+            Thread.Sleep(10000);
+        }
+
+
+        [TestMethod]
+        public void InvokeApplication()
+        {
+            string apppath = @"D:/Users/farhanShaikh/Desktop/Telerik OutlookInspiredApp for WPF.appref-ms";
+            var prc = new ProcessStartInfo(apppath);
+            app = Application.Launch(prc);
+            Thread.Sleep(80000);
+            //Getting the window object a
+            Window Tele = (Window)app.GetWindow("C:/Users/shaik/Desktop/Telerik OutlookInspiredApp for WPF.appref-ms");
+
+
+
+            //Getting the controls
+            //By Automation id
+            Button btnemail = Tele.Get<Button>(SearchCriteria.ByAutomationId("Row_5"));
+            Button btnemail2 = Tele.Get<Button>(SearchCriteria.ByAutomationId("Row_9"));
+            Button btnsearch = Tele.Get<Button>(SearchCriteria.ByAutomationId("SearchWatermarkTextBox"));
+
+
+            //Performing Clicks 
+            btnemail.Click();
+            btnsearch.Click();
+            btnemail2.Click();
+
+            Label txtResult = Tele.Get<Label>(SearchCriteria.ByAutomationId("158"));
+            //Assert will check if the expected equals Actual
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual("20", txtResult.Name);
+
+        }
+    }
+}
